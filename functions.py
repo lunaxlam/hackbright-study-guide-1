e2a946069308512ff120b5fca1fd97e2d9df6972
lunaxlam@gmail.com <lunalam@Lunas-MacBook-Pro.local>
2022-04-03 17:44:55 -0700

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,146 @@
+"""
+Skills function practice.
+
+Please read the the instructions first (separate file). Your solutions should
+go below this docstring.
+
+PART ONE:
+
+    >>> hello_world()
+    Hello World
+
+    >>> say_hi("Balloonicorn")
+    Hi Balloonicorn
+
+    >>> print_product(3, 5)
+    15
+
+    >>> repeat_string("Balloonicorn", 3)
+    BalloonicornBalloonicornBalloonicorn
+
+    >>> print_sign(3)
+    Higher than 0
+
+    >>> print_sign(0)
+    Zero
+
+    >>> print_sign(-3)
+    Lower than 0
+
+    >>> is_divisible_by_three(12)
+    True
+
+    >>> is_divisible_by_three(10)
+    False
+
+    >>> num_spaces("Balloonicorn is awesome!")
+    2
+
+    >>> num_spaces("Balloonicorn is       awesome!")
+    8
+
+    >>> total_meal_price(30)
+    34.5
+
+    >>> total_meal_price(30, .3)
+    39.0
+
+    >>> sign_and_parity(3)
+    ['Positive', 'Odd']
+
+    >>> sign_and_parity(-2)
+    ['Negative', 'Even']
+
+PART TWO:
+
+    >>> full_title("Balloonicorn")
+    'Engineer Balloonicorn'
+
+    >>> full_title("Jane Hacks", "Hacker")
+    'Hacker Jane Hacks'
+
+    >>> write_letter("Jane Hacks", "Hacker", "Balloonicorn")
+    Dear Hacker Jane Hacks, I think you are amazing! Sincerely, Balloonicorn
+
+"""
+
+###############################################################################
+
+# PART ONE
+
+# 1. Write a function called 'hello_world' that does not take any arguments and
+#    prints "Hello World".
+
+
+# 2. Write a function called 'say_hi' that takes a name as a string and
+#    prints "Hi" followed by the name.
+
+
+# 3. Write a function called 'print_product' that takes two integers and
+#    multiplies them together. Print the result.
+
+
+# 4. Write a function called 'repeat_string' that takes a string and an integer
+#    and prints the string that many times
+
+
+# 5. Write a function called 'print_sign' that takes an integer and prints
+#    "Higher than 0" if higher than zero and "Lower than 0" if lower than zero.
+#    If the integer is zero, print "Zero".
+
+
+# 6. Write a function called 'is_divisible_by_three' that takes an integer and
+#    returns a boolean (True or False), depending on whether the number is
+#    evenly divisible by 3.
+
+
+# 7. Write a function called 'num_spaces' that takes a sentence as one string
+#    and returns the number of spaces.
+
+
+# 8. Write a function called 'total_meal_price' that can be passed a meal price
+#    and a tip percentage. It should return the total amount paid
+#    (price + price * tip). **However:** passing in the tip percentage should
+#    be optional; if not given, it should default to 15%.
+
+
+# 9. Write a function called 'sign_and_parity' that takes an integer as an
+#    argument and returns two pieces of information as strings --- "Positive"
+#    or "Negative" and "Even" or "Odd". The two strings should be returned in
+#    a list.
+#
+#    Then, write code that shows the calling of this function on a number and
+#    unpack what is returned into two variables --- sign and parity (whether
+#    it's even or odd). Print sign and parity.
+
+
+###############################################################################
+
+# PART TWO
+
+# 1. Write a function called full_title that takes a name and a job title as
+#    parameters, making it so the job title defaults to "Engineer" if a job
+#    title is not passed in. Return the person's title and name in one string.
+
+# 2. Write a function called write_letter that, given a recipient name & job
+#    title and a sender name, prints the following letter:
+#
+#       Dear JOB_TITLE RECIPIENT_NAME, I think you are amazing!
+#       Sincerely, SENDER_NAME.
+#
+#    Use the function from #1 to construct the full title for the letter's
+#    greeting.
+
+
+###############################################################################
+
+# END OF PRACTICE: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print("ALL TESTS PASSED. GOOD WORK!")
+    print
---
+++ lists.py
@@ -0,0 +1,342 @@
+"""List Practice
+Edit the functions until all of the doctests pass when
+you run this file.
+"""
+
+
+def print_list(items):
+    """Print each item in the input list.
+    
+    For example::
+    
+        >>> print_list([1, 2, 6, 3, 9])
+        1
+        2
+        6
+        3
+        9
+    """
+
+    print("the wrong thing")
+
+
+def long_words(words):
+    """Return words in input list that longer than 4 characters.
+    
+    For example::
+    
+        >>> long_words(["hello", "a", "b", "hi", "muffin", "muffin"])
+        ['hello', 'muffin', 'muffin']
+    
+    (If there are duplicates, show both --- notice "muffin" appears
+    twice in output)
+    
+    If no words are longer than 4 characters, return an empty list::
+    
+        >>> long_words(["all", "are", "tiny"])
+        []
+    """
+
+    return ['the wrong thing']
+
+
+def n_long_words(words, n):
+    """Return words in list longer than `n` characters.
+    
+    For example::
+    
+        >>> n_long_words(
+        ...     ["hello", "hey", "spam", "spam", "muffin", "muffin"],
+        ...     3
+        ... )
+        ['hello', 'spam', 'spam', 'muffin', 'muffin']
+        >>> n_long_words(["I", "like", "apples", "bananas", "you"], 5)
+        ['apples', 'bananas']
+    """
+
+    return ['the wrong thing']
+
+
+def smallest_int(numbers):
+    """Find the smallest integer in a list of integers and return it.
+    **DO NOT USE** the built-in function `min()`!
+    
+    For example::
+    
+        >>> smallest_int([-5, 2, -5, 7])
+        -5
+        >>> smallest_int([3, 7, 2, 8, 4])
+        2
+    
+    If the input list is empty, return `None`::
+    
+        >>> smallest_int([]) is None
+        True
+    """
+
+    return 100
+
+
+def largest_int(numbers):
+    """Find the largest integer in a list of integers and return it.
+    **DO NOT USE** the built-in function `max()`!
+    
+    For example::
+    
+        >>> largest_int([-5, 2, -5, 7])
+        7
+        >>> largest_int([3, 7, 2, 8, 4])
+        8
+    
+    If the input list is empty, return None::
+    
+        >>> largest_int([]) is None
+        True
+    """
+
+    return 0
+
+
+def halvesies(numbers):
+    """Return list of numbers from input list, each divided by two.
+    
+    For example::
+    
+        >>> halvesies([2, 6, -2])
+        [1.0, 3.0, -1.0]
+   
+    If any of the numbers are odd, make sure you don't round off
+    the half::
+   
+        >>> halvesies([1, 5])
+        [0.5, 2.5]
+    """
+
+    return []
+
+
+def word_lengths(words):
+    """Return the length of words in the input list.
+    
+    For example::
+    
+        >>> word_lengths(["hello", "hey", "hello", "spam"])
+        [5, 3, 5, 4]
+    """
+
+    return []
+
+
+def sum_numbers(numbers):
+    """Return the sum of all of the numbers in the list.
+    Python has a built-in function, `sum()`, which already does
+    this --- but for this exercise, you should not use it.
+    
+    For example::
+    
+        >>> sum_numbers([1, 2, 3, 10])
+        16
+    
+    Any empty list should return the sum of zero::
+    
+        >>> sum_numbers([])
+        0
+    """
+
+    return None
+
+
+def mult_numbers(numbers):
+    """Return product (result of multiplication) of numbers in list.
+    
+    For example::
+    
+        >>> mult_numbers([1, 2, 3])
+        6
+    
+    Obviously, if there is a zero in input, the product is zero::
+    
+        >>> mult_numbers([10, 20, 0, 50])
+        0
+    
+    As explained at http://en.wikipedia.org/wiki/Empty_product,
+    if the list is empty, the product should be 1::
+    
+        >>> mult_numbers([])
+        1
+    """
+
+    return None
+
+
+def join_strings(words):
+    """Return a string of all input strings joined together.
+    Python has a built-in method, `list.join()` --- but for
+    this exercise, **you should not use it**.
+    
+    For example::
+    
+        >>> join_strings(["spam", "spam", "muffin", "balloonicorn"])
+        'spamspammuffinballoonicorn'
+    
+    For an empty list, you should return an empty string::
+    
+        >>> join_strings([])
+        ''
+    """
+
+    return "Not the right thing"
+
+
+def average(numbers):
+    """Return the average (mean) of the list of numbers given.
+    
+    For example::
+    
+        >>> average([2, 4])
+        3.0
+    
+    This should handle cases where the result isn't an integer::
+    
+        >>> average([2, 12, 3])
+        5.666666666666667
+    
+    There is no defined answer if the list given is empty;
+    it's fine if this raises an error when given an empty list.
+    (Think of the best way to handle an empty input list, though,
+    a feel free to provide a good solution here.)
+    """
+
+    return 0
+
+
+def join_strings_with_comma(words):
+    """Return ['list', 'of', 'words'] like "list, of, words".
+    
+    For example::
+     
+        >>> join_strings_with_comma(
+        ...     ["Labrador", "Poodle", "French Bulldog"]
+        ...     )
+        'Labrador, Poodle, French Bulldog'
+    
+    If there's only one thing in the list, it should return just that
+    thing, of course::
+   
+        >>> join_strings_with_comma(["Pretzel"])
+        'Pretzel'
+    """
+
+    return ""
+
+
+def reverse_list(items):
+    """Return the input list, reversed.
+    **Do not use** the python function `reversed()` or the method
+    `list.reverse()`.
+    
+    For example::
+    
+        >>> reverse_list([1, 2, 3])
+        [3, 2, 1]
+        >>> reverse_list(["cookies", "love", "I"])
+        ['I', 'love', 'cookies']
+    
+    You should do this without changing the original list::
+    
+        >>> orig = ["apple", "berry", "cherry"]
+        >>> reverse_list(orig)
+        ['cherry', 'berry', 'apple']
+        >>> orig
+        ['apple', 'berry', 'cherry']
+    """
+
+    return []
+
+
+def reverse_list_in_place(items):
+    """Reverse the input list `in place`.
+    Reverse the input list given, but do it "in place" --- that is,
+    do not create a new list and return it, but modify the original
+    list.
+    **Do not use** the python function `reversed()` or the method
+    `list.reverse()`.
+    
+    For example::
+    
+        >>> orig = [1, 2, 3]
+        >>> reverse_list_in_place(orig)
+        >>> orig
+        [3, 2, 1]
+        >>> orig = ["cookies", "love", "I"]
+        >>> reverse_list_in_place(orig)
+        >>> orig
+        ['I', 'love', 'cookies']
+    """
+
+    return []
+
+
+def duplicates(items):
+    """Return list of words from input list which were duplicates.
+    Return a list of words which are duplicated in the input list.
+    The returned list should be in ascending order.
+   
+    For example::
+   
+        >>> duplicates(
+        ...     ["apple", "banana", "banana", "cherry", "apple"]
+        ... )
+        ['apple', 'banana']
+        >>> duplicates([1, 2, 2, 4, 4, 4, 7])
+        [2, 4]
+   
+    You should do this without changing the original list::
+   
+        >>> orig = ["apple", "apple", "berry"]
+        >>> duplicates(orig)
+        ['apple']
+        >>> orig
+        ['apple', 'apple', 'berry']
+    """
+
+    return []
+
+
+def find_letter_indices(words, letter):
+    """Return list of indices where letter appears in each word.
+    Given a list of words and a letter, return a list of integers
+    that correspond to the index of the first occurrence of the letter
+    in that word.
+    **DO NOT** use the `list.index()` method.
+    
+    For example::
+    
+        >>> find_letter_indices(['odd', 'dog', 'who'], 'o')
+        [0, 1, 2]
+    
+    ("o" is at index 0 in "odd", is at index 1 in "dog", and at
+    index 2 in "who")
+    
+    If the letter doesn't occur in one of the words, use `None` for
+    that word in the output list. For example::
+    
+        >>> find_letter_indices(['odd', 'dog', 'who', 'jumps'], 'o')
+        [0, 1, 2, None]
+    
+    ("o" does not appear in "jumps", so the result for that input is
+    `None`.)
+    """
+
+    return []
+
+
+#####################################################################
+# END OF PRACTICE: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    result = doctest.testmod()
+    if not result.failed:
+        print("\nALL TESTS PASSED. GOOD WORK!\n")
